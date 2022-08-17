d6a69b460e3ae54e5bbd1b9948a0a60d33bdc609
unknown <cogjava3140@iiht.tech>
2022-08-17 18:16:54 +0530

    first commit

---
+++ add/src/add/Addition.java
@@ -0,0 +1,17 @@
+package add;
+import java.util.*;
+public class Addition {
+	public static void main(String args[])
+	{
+Scanner sc=new Scanner(System.in);		
+int a;
+int b;
+int sum=0;
+System.out.println("Enter the 1st number:");
+a=sc.nextInt();
+System.out.println("Enter the 2nd number:");
+b=sc.nextInt();
+sum=a+b;
+System.out.println("The summation is="+sum);
+}
+}
